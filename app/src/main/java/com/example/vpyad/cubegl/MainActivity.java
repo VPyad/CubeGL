5382f95aced2dba4bf49e05a639de793625757a2
Vadim Pyadukhov <vpyadukhov@outlook.com>
2018-04-03 21:51:02 +0300

    cube render

--- app/src/main/java/com/example/vpyad/cubegl/MainActivity.java
+++ app/src/main/java/com/example/vpyad/cubegl/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.vpyad.cubegl;
 
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.vpyad.cubegl.Renderers.OpenGLRenderer;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        GLSurfaceView view = new GLSurfaceView(this);
+        view.setRenderer(new OpenGLRenderer());
+
+        setContentView(view);
     }
 }
